c943294f0a88fb262d7ce86252795fbb137e5399
wolfy <leif@tietjenmail.de>
2019-12-26 16:55:08 +0100

    add markdown to page

--- src/components/CurriculumVitae.js
+++ src/components/CurriculumVitae.js
@@ -1,5 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Typography } from '@material-ui/core'
-const CurriculumVitae = () => <Typography variant="h2">CV</Typography>
+import Markdown from 'markdown-to-jsx'
+import cvPath from './cv.md'
+const CurriculumVitae = () => {
+	const [cv, setCv] = useState('loading...')
+	useEffect(() => {
+		fetch(cvPath)
+			.then(response => response.text())
+			.then(content => setCv(content))
+	})
+	return <Markdown>{cv}</Markdown>
+}
 
 export default CurriculumVitae
